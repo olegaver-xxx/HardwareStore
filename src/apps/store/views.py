9d4624edbf2bc94f3483a8ab8d50bd787c34b52f
oleg <oleg.bakiev.jrs@gmail.com>
2022-08-01 01:08:46 +0300

    demo-registration version

--- src/apps/store/views.py
+++ src/apps/store/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse
 # from django.http import HttpResponse
 from functools import reduce
 import operator
@@ -52,6 +56,20 @@ class IndexView(ListView):
     ordering = 'pk',
 
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(reverse('home'))
+            success_url = "/"
+    else:
+        form = UserCreationForm()
+    return render(request, 'store/register.html', {'form': form})
 # class TestBaseView(TemplateView):
 #     template_name = 'product_detail.html'
 #     model = ProductModel
--- src/main/urls.py
+++ src/main/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from apps.store.views import IndexView, ProductDetailView
+from apps.store.views import IndexView, ProductDetailView, signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='products'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product'),
+    path('signup/', signup, name='signup'),
     # path('products/<int:pk>', ProductDetailView.as_view(), name='product_details'),
     # path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
